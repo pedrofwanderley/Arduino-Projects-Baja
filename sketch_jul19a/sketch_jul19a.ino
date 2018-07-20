143f6eb49a05248dc140a37e503f7a73323afc58
pedrofwanderley <pedro.wanderley@ccc.ufcg.edu.br>
2018-07-20 11:09:24 -0300

    Changing magic numbers

--- sketch_jul19a/sketch_jul19a.ino
+++ sketch_jul19a/sketch_jul19a.ino
@@ -3,8 +3,8 @@
 #include <LiquidCrystal.h>
 
 // Inicializa o LCD
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 void setup()
 {
   Serial.begin(9600);
